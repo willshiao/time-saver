f7fe8ae05f2e96e4e47677fb0f4ea7c1e3084c09
William Shiao <willshiao@gmail.com>
2016-10-03 15:01:05 -0700

    feat: add Vue.js using vue-cli

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # time-saver
 
-A simple app to save time by calculating the amount of time saved/lost by automating a task.
+> A simple app to save time by calculating the amount of time saved/lost by automating a task.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,56 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/main.js',
   output: {
-    path: 'dist',
-    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  resolveLoader: {
+    root: path.join(__dirname, 'node_modules'),
   },
   module: {
     loaders: [
       {
-        test: /\.css$/,
-        loader: 'style!css',
+        test: /\.vue$/,
+        loader: 'vue'
       },
       {
         test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015'],
-        }
+        loader: 'babel',
+        exclude: /node_modules/
       },
       {
-        test: /\.(png|jpg|gif)$/,
-        loader: 'url-loader'
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file',
+        query: {
+          name: '[name].[ext]?[hash]'
+        }
       }
     ]
   },
-  plugins: [new HtmlWebpackPlugin({
-    title: 'babel-webpack-starter'
-  })]
-};
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ])
+}
